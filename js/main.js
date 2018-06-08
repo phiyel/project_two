5b6a96c9538b1cf5b92150200b5e29f55b127f9e
phiyel <phiyel1@gmail.com>
2018-06-08 09:15:33 -0400

    html and js updated to hide and show modal on click

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 //Jquery & JavaScripts
+$(function () {
+	//Prompt div to open
+	$('.hello-hackerYou__btn').on('click', function(){
+		console.log('fired');
+		$('.modal-hackerYou').addClass('show');
+
+	});
+
+
+	//form part
+	$('form').on('submit', function(e){
+		console.log('Testing');
+		e.preventDefault();
+	});
+
+});
