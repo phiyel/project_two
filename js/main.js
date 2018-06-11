b808d31e11071c826fa40e1f969066ff24139170
phiyel <phiyel1@gmail.com>
2018-06-11 10:56:33 -0400

    html, js and css updated to open modal window

--- js/main.js
+++ js/main.js
@@ -1,17 +1,76 @@
 //Jquery & JavaScripts
 $(function () {
+	//Variables
+	let fNameDtls;
+	let lNameDtls;
+	let emailDtls;
+	let phoneInputDtls;
+
 	//Prompt div to open
 	$('.hello-hackerYou__btn').on('click', function(){
-		console.log('fired');
-		$('.modal-hackerYou').addClass('show');
+		console.log('fired open');
+		$('.modal-background').addClass('show');
+	});
+	
+
+	//Prompt div to close
+	$('.modal-hackerYou__close').on('click', function(){
+		console.log('fired closed');
+		$('.modal-background').removeClass('show');
 
 	});
 
 
-	//form part
-	$('form').on('submit', function(e){
-		console.log('Testing');
+	//form validate
+	 $("#form").validate({
+                rules: {
+                    fName: "required",
+                    lName: "required",
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    phone: "required"
+                },
+                messages: {
+                    fName: "Please enter your first name",
+                    lName: "Please enter your last name",
+                    email: "Please enter a valid email address",
+                	phone: "Please provide a phone number"
+                },
+                 submitHandler: function() {
+			        $('.modal-hackerYou__container').hide('slow');
+			    }
+       });
+
+
+	//form submit
+	$('#form').on('submit', function(e){
+		
 		e.preventDefault();
+		
+		//Get input value
+		fNameDtls = $('input[name=fName]').val();
+		lNameDtls = $('input[name=lName]').val();
+		emailDtls = $('input[name=email]').val();
+		phoneInputDtls = $('input[type=number]').val();
+
+
+		//Output results
+	    if($(this).valid()) {
+	    	let successTitle = '<h2 class="modal-hackerYou__success--sub-title align-center">Thanks for the details!!!</h2>';
+	        let str = '';
+	        str += fNameDtls + '<br />';
+	        str += lNameDtls + '<br />';
+	        str += emailDtls + '<br />';
+	        str += phoneInputDtls + '<br />';
+	        $('.modal-hackerYou__success').append(successTitle , str);
+	    }
+
+	    return false;
 	});
+
+	
+
 
 });
